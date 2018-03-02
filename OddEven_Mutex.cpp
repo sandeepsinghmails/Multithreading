150e029b8d805a671c7384e6325307a5aefd2ff5
Sandeep Singh <sandeepsinghmails@gmail.com>
2018-03-02 08:46:10 +0530

    Create OddEven_Mutex.cpp

---
+++ OddEven_Mutex.cpp
@@ -0,0 +1,38 @@
+// Print Odd & Even Numbers Using Mutex
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <mutex>          // std::mutex
+
+std::mutex mtx;
+
+void print (int numb, char ch)
+{
+  for (int even=2, odd=1; even<=numb && odd<numb; )
+  {
+      if (ch == 'e')
+      {
+          mtx.lock();
+          std::cout << even << std::endl;
+          even += 2;
+          mtx.unlock();
+      }
+      if (ch == 'o')
+      {
+          mtx.lock();
+          std::cout << odd << std::endl;
+          odd += 2;
+          mtx.unlock();
+      }
+  }
+}
+
+int main ()
+{
+  std::thread th1 (print,30,'o');
+  std::thread th2 (print,30,'e');
+
+  th1.join();
+  th2.join();
+
+  return 0;
+}
